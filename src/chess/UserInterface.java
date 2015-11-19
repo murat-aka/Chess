17e99754b65b07de2e569b02ba8abd1df3bc5ec3
murat-aka <murat-aka@hotmail.com>
2015-11-19 19:03:09 +0000

    Graphics (part 2) update userInterface class. Add mouseClick Listeners

--- src/chess/UserInterface.java
+++ src/chess/UserInterface.java
@@ -10,12 +10,39 @@ package chess;
  * @author murat
  */
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-public class UserInterface extends JPanel{
+public class UserInterface extends JPanel implements MouseListener, MouseMotionListener{
+    static int x=0,y=0;
+    @Override
     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.setBackground(Color.yellow);
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
         g.setColor(Color.BLUE);
-        g.fillRect(20, 20, 20, 20);
+        g.fillRect(x-20, y-20, 40, 40);
         g.setColor(new Color(190,81,215));
         g.fillRect(40, 20, 80, 50);
     }
+    @Override
+    public void mouseMoved(MouseEvent e) {x=e.getX();
+        y=e.getY();
+        repaint();}
+    @Override
+    public void mousePressed(MouseEvent e) {}
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        x=e.getX();
+        y=e.getY();
+        repaint();
+    }
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+    @Override
+    public void mouseExited(MouseEvent e) {}
 }
